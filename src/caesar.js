a4c1d9e04bfbc471073ed12c1832a044f2c5499b
Sydnie “Rae” Cannon <87252453+Scannon46@users.noreply.github.com>
2021-08-20 09:17:04 -0500

    Rae's commit

--- src/caesar.js
+++ src/caesar.js
@@ -6,13 +6,51 @@
 const caesarModule = (function () {
   // you can add any code you want within this function scope
 
-  function caesar(input, shift, encode = true) {
-    // your solution code here
-  }
+  function caesar (input, shift, encode = true) {
+   if (shift === 0)
+        {return false}
+   else if (shift > 25)
+        {return false}
+   else if (shift < -25)
+        {return false}
+   else if (!shift)
+         {return false}
+   
+   if (encode === false)
+         {shift = -shift}
+         
+   const arrayInput = input.toLowerCase().split('')
+   let arrayNewChar = [];
+   let finalShiftedArray = [];
+    console.log (arrayInput)
+    const arrayChar = [];
+    for (let currentChar of arrayInput)
+    arrayChar.push(currentChar.charCodeAt())
+  
+    /*arrayChar.map((character) => {
+    const unicode = character.charCodeAt()})*/
 
-  return {
-    caesar,
-  };
-})();
+  for (let currentNum of arrayChar) {
+       if (currentNum > 96 && currentNum < 123) {
+  let newCurrentNum = currentNum + shift;
+       if (newCurrentNum < 97) {
+            newCurrentNum = newCurrentNum + 26
+    } else if (newCurrentNum > 122) {
+            newCurrentNum = newCurrentNum - 26};
+                arrayNewChar.push(newCurrentNum) ;
+    } else { arrayNewChar.push(currentNum)} }
+ for (let currentNewCode of arrayNewChar) {
+           finalShiftedArray.push(String.fromCharCode(currentNewCode))}
+    console.log(finalShiftedArray)
+ return finalShiftedArray.join("")}
+ return {caesar};
+    })();
+module.exports = {caesar: caesarModule.caesar};
 
-module.exports = { caesar: caesarModule.caesar };
+
+/*let wrappedUnicode = arrayChar.map((unicode) => {
+       if (unicode < 97) {
+            return unicode + 26}
+       if (unicode > 122) {
+            return unicode - 26}
+  })*/
